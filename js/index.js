967db86203c6fbbb35ffe0b2af4fa6b6aa4f673e
nataliercraun <Nataliercraun@Natalies-MacBook-Pro-4.local>
2018-04-23 17:29:48 -0700

    Fix js functions to show stages and times

--- js/index.js
+++ js/index.js
@@ -9,6 +9,18 @@ function show_content() {
 
 function upstage() {
 	document.querySelector("#btn_row").innerHTML = '<ul><li><button class="btns" id="timebtn" onclick="uptime()">By Time</button></li></ul>'
+	document.querySelector("#times_row").style.flexFlow = 'row wrap'; 
+	document.querySelector("#stages_row").style.flexFlow = 'row wrap'; 
+
+	var time_cards = document.getElementsByClassName("time_card"); 
+	var k; 
+	console.log(time_cards.length); 
+	for (k=0; k < time_cards.length; k++) {
+		time_cards[k].style.bottom = '-1000px'; 
+		time_cards[k].style.visibility = 'hidden'; 
+	}
+
+	
 	var elements = document.getElementsByClassName("stage_card1"); 
 	var elements2 = document.getElementsByClassName("stage_card2"); 
 	var i; 
@@ -22,5 +34,28 @@ function upstage() {
 }
 
 function uptime() {
+	
 	document.querySelector("#btn_row").innerHTML = '<ul><li><button class="btns" id="stagebtn" onclick="upstage()">By Stage</button></li></ul>'
+
+	document.querySelector("#times_row").style.flexFlow = 'row nowrap'; 
+	document.querySelector("#stages_row").style.flexFlow = 'row nowrap';
+
+	var elements = document.getElementsByClassName("stage_card1"); 
+	var elements2 = document.getElementsByClassName("stage_card2"); 
+	var i; 
+	for (i=0; i < elements.length; i++) {
+		elements[i].style.left = '-1000px'; 
+	} 
+	var j; 
+	for (j=0; j < elements2.length; j++) {
+		elements2[j].style.right = '-1000px'; 
+	}
+
+	var time_cards = document.getElementsByClassName("time_card"); 
+	var k; 
+	console.log(time_cards.length); 
+	for (k=0; k < time_cards.length; k++) {
+		time_cards[k].style.visibility = 'visible'; 
+		time_cards[k].style.bottom = '0px'; 
+	}
 }
