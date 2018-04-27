63d16abcb8edbfb7073bc96180daf24319780343
nataliercraun <Nataliercraun@Natalies-MacBook-Pro-4.local>
2018-04-27 09:32:35 -0700

    Blur filter for non-selected times

---
+++ js/time.js
@@ -0,0 +1,323 @@
+document.querySelector("#time_one").onclick = function() {
+
+	console.log("clicked"); 
+
+	if (document.querySelector("#time_one_drop").style.display == 'flex') {
+		document.querySelector("#time_one_drop").style.display = 'none';
+		document.querySelector("#time_one_drop").style.visibility = 'hidden';
+		var elements = document.getElementsByClassName("time_name"); 
+		var i; 
+		for (i=0; i < elements.length; i++) {
+			if (elements[i] != this) {
+				elements[i].style.filter = ''; 
+			}
+		} 
+	} else {
+		document.querySelector("#time_one_drop").style.display = 'flex';
+		document.querySelector("#time_one_drop").style.visibility = 'visible';
+		var elements = document.getElementsByClassName("time_name"); 
+		var i; 
+		for (i=0; i < elements.length; i++) {
+			if (elements[i] != this) {
+				elements[i].style.filter = 'blur(1px)'; 
+			}
+		} 
+	}	
+}
+
+document.querySelector("#time_two").onclick = function() {
+
+	console.log("clicked"); 
+
+	if (document.querySelector("#time_two_drop").style.display == 'flex') {
+		document.querySelector("#time_two_drop").style.display = 'none';
+		document.querySelector("#time_two_drop").style.visibility = 'hidden';
+		var elements = document.getElementsByClassName("time_name"); 
+		var i; 
+		for (i=0; i < elements.length; i++) {
+			if (elements[i] != this) {
+				elements[i].style.filter = ''; 
+			}
+		} 
+	} else {
+		document.querySelector("#time_two_drop").style.display = 'flex';
+		document.querySelector("#time_two_drop").style.visibility = 'visible';
+		var elements = document.getElementsByClassName("time_name"); 
+		var i; 
+		for (i=0; i < elements.length; i++) {
+			if (elements[i] != this) {
+				elements[i].style.filter = 'blur(1px)'; 
+			}
+		} 
+	}	
+}
+
+document.querySelector("#time_three").onclick = function() {
+
+	console.log("clicked"); 
+
+	if (document.querySelector("#time_three_drop").style.display == 'flex') {
+		document.querySelector("#time_three_drop").style.display = 'none';
+		document.querySelector("#time_three_drop").style.visibility = 'hidden';
+		var elements = document.getElementsByClassName("time_name"); 
+		var i; 
+		for (i=0; i < elements.length; i++) {
+			if (elements[i] != this) {
+				elements[i].style.filter = ''; 
+			}
+		} 
+	} else {
+		document.querySelector("#time_three_drop").style.display = 'flex';
+		document.querySelector("#time_three_drop").style.visibility = 'visible';
+		var elements = document.getElementsByClassName("time_name"); 
+		var i; 
+		for (i=0; i < elements.length; i++) {
+			if (elements[i] != this) {
+				elements[i].style.filter = 'blur(1px)'; 
+			}
+		} 
+	}	
+}
+
+document.querySelector("#time_four").onclick = function() {
+
+	console.log("clicked"); 
+
+	if (document.querySelector("#time_four_drop").style.display == 'flex') {
+		document.querySelector("#time_four_drop").style.display = 'none';
+		document.querySelector("#time_four_drop").style.visibility = 'hidden';
+		var elements = document.getElementsByClassName("time_name"); 
+		var i; 
+		for (i=0; i < elements.length; i++) {
+			if (elements[i] != this) {
+				elements[i].style.filter = ''; 
+			}
+		} 
+	} else {
+		document.querySelector("#time_four_drop").style.display = 'flex';
+		document.querySelector("#time_four_drop").style.visibility = 'visible';
+		var elements = document.getElementsByClassName("time_name"); 
+		var i; 
+		for (i=0; i < elements.length; i++) {
+			if (elements[i] != this) {
+				elements[i].style.filter = 'blur(1px)'; 
+			}
+		} 
+	}	
+}
+
+document.querySelector("#time_five").onclick = function() {
+
+	console.log("clicked"); 
+
+	if (document.querySelector("#time_five_drop").style.display == 'flex') {
+		document.querySelector("#time_five_drop").style.display = 'none';
+		document.querySelector("#time_five_drop").style.visibility = 'hidden';
+		var elements = document.getElementsByClassName("time_name"); 
+		var i; 
+		for (i=0; i < elements.length; i++) {
+			if (elements[i] != this) {
+				elements[i].style.filter = ''; 
+			}
+		} 
+	} else {
+		document.querySelector("#time_five_drop").style.display = 'flex';
+		document.querySelector("#time_five_drop").style.visibility = 'visible';
+		var elements = document.getElementsByClassName("time_name"); 
+		var i; 
+		for (i=0; i < elements.length; i++) {
+			if (elements[i] != this) {
+				elements[i].style.filter = 'blur(1px)'; 
+			}
+		} 
+	}	
+}
+
+document.querySelector("#time_six").onclick = function() {
+
+	console.log("clicked"); 
+
+	if (document.querySelector("#time_six_drop").style.display == 'flex') {
+		document.querySelector("#time_six_drop").style.display = 'none';
+		document.querySelector("#time_six_drop").style.visibility = 'hidden';
+		var elements = document.getElementsByClassName("time_name"); 
+		var i; 
+		for (i=0; i < elements.length; i++) {
+			if (elements[i] != this) {
+				elements[i].style.filter = ''; 
+			}
+		} 
+	} else {
+		document.querySelector("#time_six_drop").style.display = 'flex';
+		document.querySelector("#time_six_drop").style.visibility = 'visible';
+		var elements = document.getElementsByClassName("time_name"); 
+		var i; 
+		for (i=0; i < elements.length; i++) {
+			if (elements[i] != this) {
+				elements[i].style.filter = 'blur(1px)'; 
+			}
+		} 
+	}	
+}
+
+document.querySelector("#time_seven").onclick = function() {
+
+	console.log("clicked"); 
+
+	if (document.querySelector("#time_seven_drop").style.display == 'flex') {
+		document.querySelector("#time_seven_drop").style.display = 'none';
+		document.querySelector("#time_seven_drop").style.visibility = 'hidden';
+		var elements = document.getElementsByClassName("time_name"); 
+		var i; 
+		for (i=0; i < elements.length; i++) {
+			if (elements[i] != this) {
+				elements[i].style.filter = ''; 
+			}
+		} 
+	} else {
+		document.querySelector("#time_seven_drop").style.display = 'flex';
+		document.querySelector("#time_seven_drop").style.visibility = 'visible';
+		var elements = document.getElementsByClassName("time_name"); 
+		var i; 
+		for (i=0; i < elements.length; i++) {
+			if (elements[i] != this) {
+				elements[i].style.filter = 'blur(1px)'; 
+			}
+		} 
+	}	
+}
+
+document.querySelector("#time_eight").onclick = function() {
+
+	console.log("clicked"); 
+
+	if (document.querySelector("#time_eight_drop").style.display == 'flex') {
+		document.querySelector("#time_eight_drop").style.display = 'none';
+		document.querySelector("#time_eight_drop").style.visibility = 'hidden';
+		var elements = document.getElementsByClassName("time_name"); 
+		var i; 
+		for (i=0; i < elements.length; i++) {
+			if (elements[i] != this) {
+				elements[i].style.filter = ''; 
+			}
+		} 
+	} else {
+		document.querySelector("#time_eight_drop").style.display = 'flex';
+		document.querySelector("#time_eight_drop").style.visibility = 'visible';
+		var elements = document.getElementsByClassName("time_name"); 
+		var i; 
+		for (i=0; i < elements.length; i++) {
+			if (elements[i] != this) {
+				elements[i].style.filter = 'blur(1px)'; 
+			}
+		} 
+	}	
+}
+
+document.querySelector("#time_nine").onclick = function() {
+
+	console.log("clicked"); 
+
+	if (document.querySelector("#time_nine_drop").style.display == 'flex') {
+		document.querySelector("#time_nine_drop").style.display = 'none';
+		document.querySelector("#time_nine_drop").style.visibility = 'hidden';
+		var elements = document.getElementsByClassName("time_name"); 
+		var i; 
+		for (i=0; i < elements.length; i++) {
+			if (elements[i] != this) {
+				elements[i].style.filter = ''; 
+			}
+		} 
+	} else {
+		document.querySelector("#time_nine_drop").style.display = 'flex';
+		document.querySelector("#time_nine_drop").style.visibility = 'visible';
+		var elements = document.getElementsByClassName("time_name"); 
+		var i; 
+		for (i=0; i < elements.length; i++) {
+			if (elements[i] != this) {
+				elements[i].style.filter = 'blur(1px)'; 
+			}
+		} 
+	}	
+}
+
+document.querySelector("#time_ten").onclick = function() {
+
+	console.log("clicked"); 
+
+	if (document.querySelector("#time_ten_drop").style.display == 'flex') {
+		document.querySelector("#time_ten_drop").style.display = 'none';
+		document.querySelector("#time_ten_drop").style.visibility = 'hidden';
+		var elements = document.getElementsByClassName("time_name"); 
+		var i; 
+		for (i=0; i < elements.length; i++) {
+			if (elements[i] != this) {
+				elements[i].style.filter = ''; 
+			}
+		} 
+	} else {
+		document.querySelector("#time_ten_drop").style.display = 'flex';
+		document.querySelector("#time_ten_drop").style.visibility = 'visible';
+		var elements = document.getElementsByClassName("time_name"); 
+		var i; 
+		for (i=0; i < elements.length; i++) {
+			if (elements[i] != this) {
+				elements[i].style.filter = 'blur(1px)'; 
+			}
+		} 
+	}	
+}
+
+document.querySelector("#time_eleven").onclick = function() {
+
+	console.log("clicked"); 
+
+	if (document.querySelector("#time_eleven_drop").style.display == 'flex') {
+		document.querySelector("#time_eleven_drop").style.display = 'none';
+		document.querySelector("#time_eleven_drop").style.visibility = 'hidden';
+		var elements = document.getElementsByClassName("time_name"); 
+		var i; 
+		for (i=0; i < elements.length; i++) {
+			if (elements[i] != this) {
+				elements[i].style.filter = ''; 
+			}
+		} 
+	} else {
+		document.querySelector("#time_eleven_drop").style.display = 'flex';
+		document.querySelector("#time_eleven_drop").style.visibility = 'visible';
+		var elements = document.getElementsByClassName("time_name"); 
+		var i; 
+		for (i=0; i < elements.length; i++) {
+			if (elements[i] != this) {
+				elements[i].style.filter = 'blur(1px)'; 
+			}
+		} 
+	}	
+}
+
+document.querySelector("#time_twelve").onclick = function() {
+
+	console.log("clicked"); 
+
+	if (document.querySelector("#time_twelve_drop").style.display == 'flex') {
+		document.querySelector("#time_twelve_drop").style.display = 'none';
+		document.querySelector("#time_twelve_drop").style.visibility = 'hidden';
+		var elements = document.getElementsByClassName("time_name"); 
+		var i; 
+		for (i=0; i < elements.length; i++) {
+			if (elements[i] != this) {
+				elements[i].style.filter = ''; 
+			}
+		} 
+	} else {
+		document.querySelector("#time_twelve_drop").style.display = 'flex';
+		document.querySelector("#time_twelve_drop").style.visibility = 'visible';
+		var elements = document.getElementsByClassName("time_name"); 
+		var i; 
+		for (i=0; i < elements.length; i++) {
+			if (elements[i] != this) {
+				elements[i].style.filter = 'blur(1px)'; 
+			}
+		} 
+	}	
+}
